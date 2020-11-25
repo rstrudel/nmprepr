88db846b5ed3c16244cb61bfb2206849f2f7195e
rstrudel <rstrudel@gmail.com>
2020-11-25 01:54:26 +0100

    o3d: add show_lines method

--- mpenv/core/o3d_visualizer.py
+++ mpenv/core/o3d_visualizer.py
@@ -6,6 +6,7 @@ class Open3DVisualizer:
     def __init__(self):
         self.viz = None
         self.pcd = o3d.geometry.PointCloud()
+        self.lines = o3d.geometry.LineSet()
 
     def __del__(self):
         if self.viz is not None:
@@ -38,6 +39,34 @@ class Open3DVisualizer:
                 self.viz.update_renderer()
         else:
             o3d.visualization.draw_geometries([self.pcd])
+
+    def show_lines(self, p0, p1, blocking=False):
+        new_instance = self.viz is None
+        if new_instance and not blocking:
+            self._create_viz()
+
+        if new_instance:
+            n = p0.shape[0]
+            pcd0 = o3d.geometry.PointCloud()
+            pcd1 = o3d.geometry.PointCloud()
+            pcd0.points = o3d.utility.Vector3dVector(p0)
+            pcd1.points = o3d.utility.Vector3dVector(p1)
+            correspondances = np.stack((np.arange(n), np.arange(n)), 1)
+            self.lines = o3d.geometry.LineSet.create_from_point_cloud_correspondences(
+                pcd0, pcd1, correspondances
+            )
+        else:
+            self.lines.points = o3d.utility.Vector3dVector(np.vstack((p0, p1)))
+
+        if not blocking:
+            if new_instance:
+                self.viz.add_geometry(self.lines)
+            else:
+                self.viz.update_geometry(self.lines)
+                self.viz.poll_events()
+                self.viz.update_renderer()
+        else:
+            o3d.visualization.draw_geometries([self.lines])
 
     def show_voxels(self, voxels, blocking=True):
         if not blocking:
